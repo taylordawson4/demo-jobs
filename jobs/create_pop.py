15dd6973f5d123cd9a848cca4c92984568f2fd15
Taylor <114100001+taylordawson4@users.noreply.github.com>
2024-05-23 18:56:11 -0400

    Update create_pop.py

--- jobs/create_pop.py
+++ jobs/create_pop.py
@@ -5,7 +5,7 @@ from ipaddress import IPv4Network
 from django.contrib.contenttypes.models import ContentType
 from django.utils.text import slugify
 
-from nautobot.extras.jobs import Job
+from nautobot.extras.jobs import Job, register_jobs
 from nautobot.dcim.models import Region, Site, Device, DeviceType, DeviceRole, Interface, Cable, Rack, Platform
 from nautobot.dcim.choices import RackTypeChoices, InterfaceTypeChoices
 from nautobot.ipam.models import VLAN, IPAddress, Prefix, Role
